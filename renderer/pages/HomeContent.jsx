0813d9339c575a5d74dbe7e6568f5e8532db9c21
Mayank Rathore <mayankrathore2104@gmail.com>
2025-04-11 12:41:30 +0530

    added css

--- renderer/pages/HomeContent.jsx
+++ renderer/pages/HomeContent.jsx
@@ -4,9 +4,9 @@ import { MqttContext } from '../context/MqttContext';
 
 const HomeContent = () => {
     // Use default values to prevent errors during static generation
-    const { onPublish = () => {}, messages = [], sendTopic = '' } = 
+    const { onPublish = () => { }, messages = [], sendTopic = '' } =
         typeof window !== 'undefined' ? useContext(MqttContext) : {};
-    
+
     const [sendMessage, setSendMessage] = useState('');
 
     const handlePublish = () => {
@@ -18,27 +18,29 @@ const HomeContent = () => {
     };
 
     return <>
-        <h1 style={{ color: "black", textAlign: "center", fontWeight: "800", fontFamily: "sans-serif" }}>Home Page</h1>
-        <div className={styles['chat-container']}>
-            <div className={styles['home-area']}>
-                <h1 className={styles.msg}>Topic: {sendTopic || "Not Set"}</h1>
-                <div className={styles['chat-box']}>
-                    {messages && messages.length > 0 ? 
-                    [...messages].map((msg, index) => (
-                        <div key={index} className={`${styles['chat-message']} ${msg.type === 'sent' ? styles.sent : styles.received}`}>
-                            {msg.text}
-                        </div>
-                    )) : <div>No messages yet</div>}
+        <div className={styles['home-section']}>
+            <h1 style={{ color: "black", textAlign: "center", fontWeight: "800", fontFamily: "sans-serif" }}>Home Page</h1>
+            <div className={styles['chat-container']}>
+                <div className={styles['home-area']}>
+                    <h1 className={styles.msg}>Topic: {sendTopic || "Not Set"}</h1>
+                    <div className={styles['chat-box']}>
+                        {messages && messages.length > 0 ?
+                            [...messages].map((msg, index) => (
+                                <div key={index} className={`${styles['chat-message']} ${msg.type === 'sent' ? styles.sent : styles.received}`}>
+                                    {msg.text}
+                                </div>
+                            )) : <div>No messages yet</div>}
+                    </div>
                 </div>
-            </div>
 
-            {/* Publisher */}
-            <div className={styles['home-container']}>
-                <div className={styles['home-form']}>
-                    <h3 className={styles['set-h3']}>Publisher</h3>
-                    <input type="text" name="text" placeholder='enter text to send' value={sendMessage}
-                        onChange={(e) => setSendMessage(e.target.value)} />
-                    <button onClick={handlePublish} type='button' className={styles.btn}>Send</button>
+                {/* Publisher */}
+                <div className={styles['home-container']}>
+                    <div className={styles['home-form']}>
+                        <h3 className={styles['set-h3']}>Publisher</h3>
+                        <input type="text" name="text" placeholder='enter text to send' value={sendMessage}
+                            onChange={(e) => setSendMessage(e.target.value)} />
+                        <button onClick={handlePublish} type='button' className={styles.btn}>Send</button>
+                    </div>
                 </div>
             </div>
         </div>
